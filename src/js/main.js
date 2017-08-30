7bb9d9c95904506a90bc441849f183c723acfc07
ankith sadashiva <ankith8@gmail.com>
2017-08-31 00:44:56 +0530

    cleaned up code

--- src/js/main.js
+++ src/js/main.js
@@ -3,28 +3,25 @@
 console.log("Testing");
 
 $(document).ready(function() {
-    init();
+    $(".faa-burst").hide();
+    checkNav();
 });
 
 
+// Open and Close Navigation functions for Mobile
 function openNav() {
     $("#mySidenav").css("width","246px");
     $(".closebtn").show();
     $("body").css("background-color","rgba(0,0,0,0.4)");
-    console.log("open");
 }
 
 function closeNav() {
     $("#mySidenav").css("width","0");
     $("body").css("background-color","white");
     $(".closebtn").hide();
-    console.log("close");
 }
 
-function init(){
-    checkNav();
-}
-
+// To check if the navbar should be collapsed or fixed
 function checkNav(){
     if (window.matchMedia('(max-width: 768px)').matches)
     {
@@ -37,9 +34,23 @@ function checkNav(){
 }
 
 $(window).on("resize", function () {
-    
     checkNav();
-
 });
-    
 
+// Clicking + Sign Animation
+$(".search-input").click(function () {
+    // Remove any old one
+    $(".search-anim").removeClass("rot");
+    // Add the class to show rotation animation
+    $(".search-anim").addClass("rot");
+  });
+// On typing show ripple effect animation
+$('.search-input').on('input', function() {
+    $(".faa-burst").show();
+    $(".search-anim").hide();
+
+    setTimeout(function() {
+        $(".faa-burst").hide();
+        $(".search-anim").show();
+    }, 1500);
+});
